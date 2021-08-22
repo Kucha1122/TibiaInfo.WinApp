09e4c8e81d4c07a9cbd525e6f0f5509205b78737
Michał K <uyu1@O2.pl>
2021-08-23 00:34:04 +0200

    Conifugred basic of system tray application

---
+++ TibiaInfo.WinApp/ApplicationForm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TibiaInfo.WinApp
+{
+    public partial class ApplicationForm : Form
+    {
+        private readonly FileSystemWatcher _fileSystemWatcher;
+        public ApplicationForm()
+        {
+            _fileSystemWatcher = new FileSystemWatcher();
+            InitializeComponent();
+            CenterToParent();
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            showApplicationForm();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ApplicationForm_Move(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            showApplicationForm();
+        }
+
+
+        private void showApplicationForm()
+        {
+            Show();
+            ShowInTaskbar = true;
+            notifyIcon1.Visible = true;
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
