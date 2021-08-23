a7ece7e8b91daacf6ad70c19368c7c7e7144026f
Michał K <uyu1@O2.pl>
2021-08-23 02:42:20 +0200

    Created custom config file manager

--- TibiaInfo.WinApp/ApplicationForm.cs
+++ TibiaInfo.WinApp/ApplicationForm.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
 using System.Windows.Forms;
+using TibiaInfo.WinApp.Config;
 
 namespace TibiaInfo.WinApp
 {
     public partial class ApplicationForm : Form
     {
         private readonly FileSystemWatcher _fileSystemWatcher;
+        private readonly ConfigurationFileManager _configurationFileManager;
+        private readonly Settings _settings;
+
         public ApplicationForm()
         {
-            _fileSystemWatcher = new FileSystemWatcher();
             InitializeComponent();
             CenterToParent();
+            
+            _fileSystemWatcher = new FileSystemWatcher();
+            _configurationFileManager = new ConfigurationFileManager();
+            _settings = _configurationFileManager.loadConfigFile();
+            loadConfigurationSettings();
         }
 
         private void showToolStripMenuItem_Click(object sender, EventArgs e)
@@ -21,6 +30,8 @@ namespace TibiaInfo.WinApp
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            _configurationFileManager.saveConfigFile(CharacterNameLabel.Text,
+                HuntingSessionScannerCheckbox.Checked);
             Application.Exit();
         }
 
@@ -36,14 +47,61 @@ namespace TibiaInfo.WinApp
         {
             showApplicationForm();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var setNameDialog = new SetNameDialog();
 
-
+            if (setNameDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                CharacterNameLabel.Text = setNameDialog.text
+            }
+        }
+        
+        private void ApplicationForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _configurationFileManager.saveConfigFile(CharacterNameLabel.Text,
+                HuntingSessionScannerCheckbox.Checked);
+        }
+        private void HuntingSessionScannerCheckbox_Click(object sender, EventArgs e)
+        {
+            _configurationFileManager.setHuntingSessionScanner(HuntingSessionScannerCheckbox.Checked); 
+        }
+        
+        
         private void showApplicationForm()
         {
             Show();
             ShowInTaskbar = true;
             notifyIcon1.Visible = true;
             WindowState = FormWindowState.Normal;
+        }
+        
+        private void loadConfigurationSettings()
+        {
+            try
+            {
+                HuntingSessionScannerCheckbox.Checked = _settings.HuntingSessionScanner;
+                CharacterNameLabel.Text = _settings.CharacterName;
+                DirectoryLocationLabel.Text = _settings.TibiaDirectoryLocation;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong with configuration file.");
+            }
+        }
+
+        private void ChangeDirectoryButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowNewFolderButton = true;
+
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                DirectoryLocationLabel.Text = folderBrowserDialog.SelectedPath;
+                _configurationFileManager.setTibiaDirectoryLocation(folderBrowserDialog.SelectedPath);
+            }
         }
     }
 }
---
+++ TibiaInfo.WinApp/ConfigurationFileManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+using TibiaInfo.WinApp.Config;
+
+namespace TibiaInfo.WinApp
+{
+    public class ConfigurationFileManager
+    {
+        private readonly Configuration _configuration;
+        private readonly KeyValueConfigurationCollection _settings;
+
+        public ConfigurationFileManager()
+        {
+            _configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            _settings = _configuration.AppSettings.Settings;
+        }
+        
+        public Settings loadConfigFile()
+        {
+            var settings = new Settings();
+
+            settings.CharacterName = _settings["CharacterName"].Value;
+            settings.HuntingSessionScanner = Convert.ToBoolean(_settings["HuntingSessionScanner"].Value);
+            settings.TibiaDirectoryLocation = _settings["TibiaDirectoryLocation"].Value;
+
+            return settings;
+        }
+
+        public void saveConfigFile(string characterName, bool value)
+        {
+            try
+            {
+                _settings["CharacterName"].Value = characterName;
+                _settings["HuntingSessionScanner"].Value = Convert.ToString(value);
+
+                _configuration.Save(ConfigurationSaveMode.Modified);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setCharacterName(string characterName)
+        {
+            try
+            {
+                _settings["CharacterName"].Value = characterName;
+                _configuration.Save(ConfigurationSaveMode.Modified);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void setHuntingSessionScanner(bool value)
+        {
+            try
+            {
+                _settings["HuntingSessionScanner"].Value = Convert.ToString(value);
+                _configuration.Save(ConfigurationSaveMode.Modified);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                throw ex;
+            }
+        }
+        
+        public void setTibiaDirectoryLocation(string directoryLocation)
+        {
+            try
+            {
+                _settings["TibiaDirectoryLocation"].Value = Convert.ToString(directoryLocation);
+                _configuration.Save(ConfigurationSaveMode.Modified);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
